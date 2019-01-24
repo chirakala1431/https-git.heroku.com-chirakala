acc2e85f4ae083ad47c7b9be7282c6b2b5a6225d
hema vami <krish.chirakal@gmail.com>
2019-01-24 19:27:59 +0530

    make it better

--- src/app/tran/tran.component.ts
+++ src/app/tran/tran.component.ts
@@ -1,6 +1,6 @@
 import { Component,ViewChild,ElementRef, OnInit, Inject } from '@angular/core';
 import { DataService } from '../data.service';
-import * as trans from 'jspdf';
+//import * as trans from 'jspdf';
 @Component({
   selector: 'app-tran',
   templateUrl: './tran.component.html',
@@ -17,18 +17,18 @@ export class TranComponent implements OnInit {
   {
     this.ds.adminTrans().subscribe(temp=>{this.data=temp});
   }
-  trans(v)
-  {
-    var pdf=new trans();
-    let handler={
-      '#editor':function(element,renderer)
-      {
-        return true;
-      }
-    };
-    let table=this.table.nativeElement;
-    pdf.fromHTML(table.innerHTML,15,15,{'width':200,'elementHandlers':handler});
-    pdf.save("transaction.pdf");
-  }
+//   trans(v)
+//   {
+//     var pdf=new trans();
+//     let handler={
+//       '#editor':function(element,renderer)
+//       {
+//         return true;
+//       }
+//     };
+//     let table=this.table.nativeElement;
+//     pdf.fromHTML(table.innerHTML,15,15,{'width':200,'elementHandlers':handler});
+//     pdf.save("transaction.pdf");
+//   }
 
 }
